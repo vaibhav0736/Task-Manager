b73ef7546b667e9f2e73d137633a28a9a8599adc
Vaibhav <vaibhavawasthi736@gmail.com>
2025-07-05 18:41:16 +0530

    Fix CORS issue with correct frontend origin

--- backend/server.js
+++ backend/server.js
@@ -16,6 +16,7 @@ const app= express();
         origin: process.env.CLIENT_URL,
         methods:[ "GET", "POST", "PUT", "DELETE"],
       allowedHeaders: ["Content-Type", "Authorization"],
+       credentials: true,
     })
   );
   //connect to db
