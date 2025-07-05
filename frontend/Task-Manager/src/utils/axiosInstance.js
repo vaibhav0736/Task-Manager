9b8b5876aac9f74b1efb612eecb268108f4089e5
Vaibhav <vaibhavawasthi736@gmail.com>
2025-07-05 18:28:12 +0530

    Increased Axios timeout to 20s

--- frontend/Task-Manager/src/utils/axiosInstance.js
+++ frontend/Task-Manager/src/utils/axiosInstance.js
@@ -4,7 +4,7 @@ import { BASE_URL } from "./apiPaths";
 
 const axiosInstance=axios.create({
     baseURL:BASE_URL,
-    timeout:10000,
+    timeout:20000,
     headers:{
         'Content-Type':'application/json',
         Accept:"application/json"
